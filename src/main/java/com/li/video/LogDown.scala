23e9b99eb1e8b743d7b52228926f7ecce9404650
e <sishengqikuo_xx@163.com>
2019-01-15 15:41:14 +0800

    2019-01-15 ...1

--- src/main/java/com/li/video/LogDown.scala
+++ src/main/java/com/li/video/LogDown.scala
@@ -18,6 +18,8 @@ case class LogDown(cv: String,
                    uname: String,
                    netclassid: Int,
                    syllabusid: Int,
+                   title: String,
+                   syname: String,
                    playtime: Int,
                    playhour: Int,
                    playmonth: String,
@@ -49,6 +51,7 @@ object LogDown {
       l = System.currentTimeMillis() - 2 * 24 * 60 ^ 60 * 1000L
     }
 
+//    l = 1544371200000L
     val warehouseLocation = new File("spark-warehouse").getAbsolutePath
 
     val weekStart = getWeekStart(yyyyMMdd.format(new Date(l)))
@@ -65,7 +68,7 @@ object LogDown {
 
     val conf = new SparkConf()
       .setAppName("LogDown")
-    //      .setMaster("local[6]")
+      .setMaster("local[6]")
 
     val session = SparkSession.builder.config(conf).config("spark.sql.warehouse.dir", warehouseLocation).enableHiveSupport().getOrCreate()
 
@@ -84,8 +87,11 @@ object LogDown {
     val syllabus = session.read.format("jdbc").options(options).load
     syllabus.createOrReplaceTempView("syllabus")
 
-    val map = sc.broadcast(session.sql(" " +
-      " SELECT id as syllabusId,net_class_id as netClassId from syllabus ")
+    val sy = session.sql(" " +
+      " SELECT id as syllabusId,net_class_id as netClassId,name from syllabus ")
+    sy.cache()
+
+    val map = sc.broadcast(sy
       .mapPartitions {
         ite =>
           val arr = new ArrayBuffer[(Long, Long)]()
@@ -117,6 +123,66 @@ object LogDown {
 
       }.rdd.collectAsMap())
 
+    val map2 = sc.broadcast(sy
+      .mapPartitions {
+        ite =>
+          val arr = new ArrayBuffer[(Long, String)]()
+
+          while (ite.hasNext) {
+            val r = ite.next()
+
+            val a = r.get(0).getClass.getName match {
+              case "java.lang.Integer" =>
+                r.getAs[Int](0).longValue()
+              case "java.lang.Long" =>
+                r.getAs[Long](0)
+              case _ =>
+                throw new ClassCastException
+            }
+
+            val b = r.getAs[String]("name")
+
+            arr += Tuple2(a, b)
+          }
+          arr.iterator
+
+      }.rdd.collectAsMap())
+
+
+    val df_sqlServer = session.read.format("jdbc")
+      .option("url", "jdbc:sqlserver://192.168.100.211:11433")
+      .option("databaseName", "htolmain")
+      .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver")
+      .option("dbtable", "NetClasses")
+      .option("user", "htol_new")
+      .option("password", "QWE_ht@1435#YTu").load()
+
+    val title = sc.broadcast(
+      df_sqlServer.mapPartitions {
+        ite =>
+          val arr = new ArrayBuffer[(Long, String)]()
+
+          while (ite.hasNext) {
+
+            val r = ite.next()
+            val a = r.get(0).getClass.getName match {
+              case "java.lang.Integer" =>
+                r.getAs[Int](0).longValue()
+              case "java.lang.Long" =>
+                r.getAs[Long](0)
+              case _ =>
+                throw new ClassCastException
+            }
+
+            val b = r.getAs[String]("Title")
+
+            arr += Tuple2(a, b)
+          }
+
+          arr.iterator
+      }.rdd.collectAsMap())
+
+
     val videoLog = sc.textFile(baseHdfs + arr(0))
       .++(sc.textFile(baseHdfs + arr(1)))
       .++(sc.textFile(baseHdfs + arr(2)))
@@ -135,8 +201,10 @@ object LogDown {
       ite =>
         val arr = new ArrayBuffer[LogDown]()
         val php = map.value
+        val php2 = map2.value
 
         val w = ws.value
+        val tital = title.value
 
         val yyyy_mm_dd_hh_mm = new SimpleDateFormat("yyyy_MM_dd_HH_mm")
         val HH_yyyyMM_yyyyMMdd = new SimpleDateFormat("HH,yyyyMM,yyyyMMdd")
@@ -164,6 +232,9 @@ object LogDown {
               if (syllabusId != null) {
 
                 val netClassId = php.getOrElse(parseLong(syllabusId).get, 0L).intValue()
+                val ti = tital.getOrElse(netClassId.longValue(), "")
+
+                val syname= php2.getOrElse(parseLong(syllabusId).get, "")
 
                 arr += LogDown(
                   cv,
@@ -171,6 +242,8 @@ object LogDown {
                   userName,
                   netClassId,
                   parseInt(syllabusId).get,
+                  ti,
+                  syname,
                   parseInt(userPlayTime).get,
                   parseInt(t(0)).get,
                   t(1),
